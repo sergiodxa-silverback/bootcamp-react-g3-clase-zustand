a077f9cfd7aa259f5f61f247e6791511d8ea97a7
Sergio Xalambrí <hello@sergiodxa.com>
2024-02-21 19:54:33 -0500

    Remove useless validation

--- src/routes/meal.$mealId.tsx
+++ src/routes/meal.$mealId.tsx
@@ -13,24 +13,20 @@ export function Component() {
   return (
     <div className="p-2">
       <h2 className="text-4xl font-lato font-bold">Recetario</h2>
-      {Object.keys(meal)?.length ? (
-        <div className="flex py-8 px-4">
-          <div className="w-1/4 flex flex-col items-start mb-2">
-            <label className="font-lato font-bold text-xl">
-              {meal.strMeal}
-            </label>
-            <img
-              src={meal?.strMealThumb}
-              alt={meal?.strMeal}
-              className="w-80 max-w-6xl"
-            />
-          </div>
-          <div className="w-3/4 flex flex-col items-start p-4">
-            <label className="text-lg font-lato">Instrucciones</label>
-            <p className="text-left font-lato">{meal?.strInstructions}</p>
-          </div>
+      <div className="flex py-8 px-4">
+        <div className="w-1/4 flex flex-col items-start mb-2">
+          <label className="font-lato font-bold text-xl">{meal.strMeal}</label>
+          <img
+            src={meal?.strMealThumb}
+            alt={meal?.strMeal}
+            className="w-80 max-w-6xl"
+          />
         </div>
-      ) : undefined}
+        <div className="w-3/4 flex flex-col items-start p-4">
+          <label className="text-lg font-lato">Instrucciones</label>
+          <p className="text-left font-lato">{meal?.strInstructions}</p>
+        </div>
+      </div>
     </div>
   );
 }
